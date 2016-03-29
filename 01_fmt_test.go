96251705aff013ca50ac88f85c57c0e779d12f77
Daniel Bornkessel <daniel.bornkessel@innoq.com>
2016-03-29 08:51:26 +0200

    01 exercise

---
+++ 01_fmt_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os/exec"
+	"testing"
+)
+
+// just a test that is formatted incorrectly
+func TestFormatting(t *testing.T) {
+cmd := exec.Command("gofmt", "-l", "-e", ".")
+out, err := cmd.CombinedOutput()
+if err != nil {
+	t.Errorf("Fehler beim ausführen von `gofmt`: %s", err.Error())
+}
+
+if len(out) > 0 {
+	t.Errorf("folgende Dateien sind nicht korrekt formatiert: %s", out)
+}
+}
