1cca2c1eda58a64667b977ef4d3a39013061212a
Daniel Bornkessel <daniel.bornkessel@innoq.com>
2016-04-01 17:04:23 +0200

    01 solution

--- 01_fmt_test.go
+++ 01_fmt_test.go
@@ -7,13 +7,13 @@ import (
 
 // just a test that is formatted incorrectly
 func TestFormatting(t *testing.T) {
-cmd := exec.Command("gofmt", "-l", "-e", ".")
-out, err := cmd.CombinedOutput()
-if err != nil {
-	t.Errorf("Fehler beim ausführen von `gofmt`: %s", err.Error())
-}
+	cmd := exec.Command("gofmt", "-l", "-e", ".")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Errorf("Fehler beim ausführen von `gofmt`: %s", err.Error())
+	}
 
-if len(out) > 0 {
-	t.Errorf("folgende Dateien sind nicht korrekt formatiert: %s", out)
-}
+	if len(out) > 0 {
+		t.Errorf("folgende Dateien sind nicht korrekt formatiert: %s", out)
+	}
 }
